b4524e7116bcb877c4c77931ff48545fe5cc2153
Abhiraj Dutta <abhiraj@amac.local>
2026-04-17 18:01:22 +0530

    Removed redundant comments

--- apply.py
+++ apply.py
@@ -6,45 +6,40 @@ import matplotlib.pyplot as plt
 from model_architecture import CNN
 import torchvision.transforms.functional as TF
 
-# 1. Load model
+
 model = CNN()
 checkpoint = torch.load("model.pth", map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint['model_state_dict'])
 model.eval()
 
-# 2. Transform (match MNIST exactly)
+
 transform = transforms.ToTensor()
 
-# 3. Load image
+
 image_path = "3.png"
 image = Image.open(image_path).convert("L")
 
 
-# IMPORTANT: resize with correct interpolation
+
 image = image.resize((28, 28), Image.NEAREST)
 
-# 4. Convert to tensor
-image = transform(image)  # [1, 28, 28], values [0,1]
 
-# 🔥 FIX 1: Ensure correct polarity
-# MNIST = white digit (high values) on black (low values)
-# If your digit looks dark on light background → invert
+image = transform(image)
+
+
 if image.mean() > 0.5:
     image = 1 - image
 
-# 🔥 FIX 2: Normalize like MNIST (this stabilizes predictions)
-# image = (image - 0.1307) / 0.3081
-# print(image.min().item(), image.max().item(), image.mean().item())
-# 5. Show image
+
 plt.imshow(image.squeeze(), cmap='gray')
 plt.title("Input to Model (after preprocessing)")
 plt.axis('off')
 # plt.show()
 
-# 6. Add batch dimension
+
 image = image.unsqueeze(0)  # [1, 1, 28, 28]
 
-# 7. Predict
+
 with torch.no_grad():
     output = model(image)
     pred = output.argmax(dim=1).item()
